1cc4494cbcafa36645d7ca7b3f8e2751cbaadbdd
Kacper Wikieł <random8902@gmail.com>
2018-10-04 11:00:01 +0200

    Postgresl heroku support

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,25 @@
 
 ActiveRecord::Schema.define(version: 2018_09_19_062737) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "networths", force: :cascade do |t|
     t.decimal "value"
     t.datetime "date"
@@ -19,4 +38,5 @@ ActiveRecord::Schema.define(version: 2018_09_19_062737) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
